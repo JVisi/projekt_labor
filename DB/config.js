bca2874a6d39bd19286ad87425e564e80835d84c
Patrik Visi <justvisitf@gmail.com>
2021-10-31 21:58:38 +0100

    heroku deploy

--- DB/config.js
+++ DB/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+//require('dotenv').config()
 const sequelize = require("sequelize");
 
 const seq = new sequelize.Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
